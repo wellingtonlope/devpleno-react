c1f482625ee83524200b25ddafd5a9ed4bca8276
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-14 20:45:38 -0400

    Comentários: Test do Comment

--- comentarios/src/Comment.js
+++ comentarios/src/Comment.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
-const Comment = ({ comment }) =>
-  <div>Comentário: {comment.comment}</div>
+const Comment = ({ comment }) => {
+  let description = 'vazio'
+  if (comment && comment.comment) {
+    description = comment.comment
+  }
+
+  return (
+    <div>Comentário: {description}</div>
+  )
+}
 
 export default Comment

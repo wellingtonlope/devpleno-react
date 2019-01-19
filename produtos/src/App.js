c4f36e2583a0477b0dc55095367113b0a4d1d0c6
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-19 10:17:36 -0400

    Estrutura inicial

--- produtos/src/App.js
+++ produtos/src/App.js
@@ -3,7 +3,24 @@ import React, { Component } from 'react'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <nav className='navbar navbar-inverse'>
+          <div className='container'>
+            <div className='navbar-header'>
+              <a href='/' className='navbar-brand'>
+                Gerenciador de Produtos
+              </a>
+            </div>
+            <ul className='nav navbar-nav'>
+              <li><a href='/'>Home</a></li>
+              <li><a href='/'>Produtos</a></li>
+              <li><a href='/'>Sobre</a></li>
+            </ul>
+          </div>
+        </nav>
+        <div className='container'>
+          <h1>Gerenciador de Produtos</h1>
+        </div>
       </div>
     )
   }
